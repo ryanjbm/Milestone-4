3d841f2ab0d00edf90ed514db5a72d949a2efb8c
ryanjbm <rjbyrne22@gmail.com>
2021-07-15 10:57:25 +0000

    Ability to adjust amount of items in bag, remove items and total updates in real time

--- bag/views.py
+++ bag/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
+
 
 def view_bag(request):
     """ A view to renders the shoping bag contents """
     
     return render(request, 'bag/bag.html')
+
 
 def add_to_bag(request, item_id):
 
@@ -20,3 +22,40 @@ def add_to_bag(request, item_id):
     
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+   
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
