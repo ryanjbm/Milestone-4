5203a94fe337ddaf1b6aaab237a22c66fc08576a
ryanjbm <rjbyrne22@gmail.com>
2021-07-22 10:11:41 +0000

    added stripe payments to checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key':'pk_test_51JFyelDi7sEj6OcN40FfeTMImvzcVni0xAu2nsnd0RQDqsN2sc9d0h87l6skFGMLWymOPurCENBEdOVSWYeTbMgb00P6WZdELd',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
