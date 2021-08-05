2601165836ea16eca30aebf6cde8998aa6fce48a
ryanjbm <rjbyrne22@gmail.com>
2021-08-05 11:15:12 +0000

    Settings typo change

--- retro_fc/settings.py
+++ retro_fc/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -123,7 +123,7 @@ WSGI_APPLICATION = 'retro_fc.wsgi.application'
 
 
 if 'DATABASE_URL' in os.environ:
-    DATABASE = {
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
