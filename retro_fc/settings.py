edc4a0d07a80389e147bb3371bee672edd649a79
ryanjbm <rjbyrne22@gmail.com>
2021-08-25 09:48:50 +0000

    Code Validation

--- retro_fc/settings.py
+++ retro_fc/settings.py
@@ -14,18 +14,14 @@ from pathlib import Path
 import os
 import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+
+DEBUG = False
 
 ALLOWED_HOSTS = ['retro-fc.herokuapp.com', 'localhost']
 
